2aa7d72134bdabf3c8ee1036fa62c51f1917a13c
Elton Li <elton.li@ubenefit.net>
2023-06-21 23:13:46 -0500

    Spring Security Intro

---
+++ ILProperty/src/main/java/com/heyprojecthub/ilproperty/IlPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.heyprojecthub.ilproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlPropertyApplication.class, args);
+    }
+
+}
---
+++ ILProperty/src/test/java/com/heyprojecthub/ilproperty/IlPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyprojecthub.ilproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlPropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

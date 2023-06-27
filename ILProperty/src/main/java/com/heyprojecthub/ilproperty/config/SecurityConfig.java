aabadd18e98ca2ee9e19f4100c48782f691b7a7e
Elton Li <elton.li@ubenefit.net>
2023-06-26 21:41:53 -0500

    Added Password Encoder

---
+++ ILProperty/src/main/java/com/heyprojecthub/ilproperty/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.heyprojecthub.ilproperty.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- ILProperty/src/test/java/com/heyprojecthub/ilproperty/IlPropertyApplicationTests.java
+++ ILProperty/src/test/java/com/heyprojecthub/ilproperty/IlPropertyApplicationTests.java
@@ -5,6 +5,7 @@ import com.heyprojecthub.ilproperty.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
@@ -18,5 +19,12 @@ class IlPropertyApplicationTests {
     public void testUserMapper() {
         List<User> users = userMapper.selectList(null);
         System.out.println(users);
+    }
+
+    @Test
+    public void testPasscodeEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String encode = bCryptPasswordEncoder.encode("123456");
+        System.out.println(encode);
     }
 }

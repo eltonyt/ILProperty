b11796ccb6993275f3b0fe4002cae13e8a8c67bf
Elton Li <elton.li@ubenefit.net>
2023-06-26 21:51:20 -0500

    tested jwt token encryption & decryption

--- ILProperty/src/main/java/com/heyprojecthub/ilproperty/utils/JwtUtil.java
+++ ILProperty/src/main/java/com/heyprojecthub/ilproperty/utils/JwtUtil.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 public class JwtUtil {
 
     public static final Long JWT_TTL = 24 * 60 * 60 * 1000L;// 1 Hour
-    public static final String JWT_KEY = "elton";
+    public static final String JWT_KEY = "ilbg";
 
     public static String getUUID(){
         String token = UUID.randomUUID().toString().replaceAll("-", "");
@@ -55,9 +55,13 @@ public class JwtUtil {
     }
 
     public static void main(String[] args) throws Exception {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjYWM2ZDVhZi1mNjVlLTQ0MDAtYjcxMi0zYWEwOGIyOTIwYjQiLCJzdWIiOiJzZyIsImlzcyI6InNnIiwiaWF0IjoxNjM4MTA2NzEyLCJleHAiOjE2MzgxMTAzMTJ9.JVsSbkP94wuczb4QryQbAke3ysBDIL5ou8fWsbt_ebg";
-        Claims claims = parseJWT(token);
-        System.out.println(claims);
+        String jwt = createJWT("1234");
+        System.out.println(jwt);
+
+        Claims claims = parseJWT(jwt);
+        String subject = claims.getSubject();
+        System.out.println(subject);
+//        Claims claims = parseJWT(token);
     }
 
     public static SecretKey generalKey() {
